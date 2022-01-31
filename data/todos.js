065ea383f357dd1b2f56dd6e4e125c271cd4f67d
Holmkvists <adamholmkvists@gmail.com>
2022-01-31 15:16:02 +0100

    todos initial data set

---
+++ data/todos.js
@@ -0,0 +1,24 @@
+const todosArray = [
+  {
+    id: 1,
+    created: "2022-1-24 19:29",
+    description: "Pay the rent tomorrow.",
+    completed: false,
+  },
+  {
+    id: 2,
+    created: "2022-1-28 09:14",
+    description:
+      "Turn in all reports with deadline for today before leaving the office.",
+    completed: false,
+  },
+
+  {
+    id: 3,
+    created: "2022-1-30 08:42",
+    description: "Do the weekly shopping during the day.",
+    completed: false,
+  },
+];
+
+module.exports = todosArray;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const todosArray = require("./data/todos");
 
 const app = express();
 
@@ -13,12 +14,24 @@ app.engine(
 
 app.set("view engine", "hbs");
 
-app.get("/", (req, res) => {
-  res.send("it works");
-});
+function getDateTime() {
+  let today = new Date();
+  let date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  let time = today.getHours() + ":" + today.getMinutes();
+  let dateTime = date + " " + time;
+
+  return dateTime;
+}
+
+console.log(todosArray);
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("home", { todosArray });
+});
 
 app.listen(8000, () => {
   console.log("http://localhost:8000/");
