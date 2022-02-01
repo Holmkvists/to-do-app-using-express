e3d64d188b758cce093fb041a3f2b56e8dc7226c
Holmkvists <adamholmkvists@gmail.com>
2022-02-01 12:59:37 +0100

    crud in progress

--- index.js
+++ index.js
@@ -14,14 +14,26 @@ app.engine(
 
 app.set("view engine", "hbs");
 
+function createNewId(array) {
+  let newId = 0;
+
+  for (const item of array) {
+    if (item.id > newId) {
+      newId = item.id;
+    }
+  }
+
+  return newId + 1;
+}
+
 function getDateTime() {
   let today = new Date();
   let date =
     today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
   let time = today.getHours() + ":" + today.getMinutes();
-  let dateTime = date + " " + time;
+  let currentDateTime = date + " " + time;
 
-  return dateTime;
+  return currentDateTime;
 }
 
 console.log(todosArray);
@@ -31,6 +43,35 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("home", { todosArray });
+});
+
+app.post("/", (req, res) => {
+  const id = createNewId(todosArray);
+
+  const newTodo = {
+    id: id,
+    description: req.body.description,
+    created: getDateTime(),
+    completed: false,
+  };
+
+  console.log(newTodo);
+  todosArray.push(newTodo);
+  res.redirect("/");
+});
+
+app.get("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todo = todosArray.find((t) => t.id === id);
+
+  res.render("todo-detailed", todo);
+});
+
+app.get("/todos/:id/edit", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todo = todosArray.find((t) => t.id === id);
+
+  res.render("todo-edit", todo);
 });
 
 app.listen(8000, () => {
